0855df87266e027727da1c0ef877f9721eab7e2b
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-18 12:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const browserName = navigator.appName;
+const browserVersion = navigator.appVersion;
+
+ const browserInfoDiv = document.getElementById('browser-info');
+
+const message = "You are using " + browserName + " version " + browserVersion;
+
+browserInfoDiv.textContent = message;
